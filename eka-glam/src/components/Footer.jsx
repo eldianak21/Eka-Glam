087854334f55566a528a17dc2a6bafddd7356e67
Eldana Kibru <156713119+eldianak21@users.noreply.github.com>
2024-12-25 23:34:20 +0300

    footer is updated

--- eka-glam/src/components/Footer.jsx
+++ eka-glam/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4 text-center">
+    <footer className="bg-white text-pink-500 py-4 text-center">
       <p>&copy; {new Date().getFullYear()} Eka Glam. All rights reserved.</p>
     </footer>
   );
